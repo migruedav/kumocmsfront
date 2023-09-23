0f0d24a8e41f874e16d9ac5dea89d56f753e393e
migruedav <migruedav@hotmail.com>
2023-09-23 16:58:34 -0600

    update CalCard.jsx

--- src/components/CalCard.jsx
+++ src/components/CalCard.jsx
@@ -69,8 +69,8 @@ function CalCard({ Nickname, Foto, id, Grupo }) {
           fecha: date,
           koins: total,
           programa: programa,
-          puntualidad: puntualidad,
           uniforme: uniforme,
+          puntualidad: puntualidad,
         },
       ])
       .select();
