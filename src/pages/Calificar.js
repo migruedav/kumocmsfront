fd43f07a0140501229fabfe975488e8ab5c9e7f9
Miguel Rueda <migruedav@hotmail.com>
2025-01-07 20:40:17 -0600

    Agregar botón para subir y mejorar el manejo del scroll en Calificar

--- src/pages/Calificar.js
+++ src/pages/Calificar.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { supabase } from "../supabase";
 import CalCard from "../components/CalCard";
 import GroupsChips from "../components/GroupsChips";
-
 
 function Calificar() {
   const [data, setData] = React.useState([]);
   const [active, setActive] = React.useState(5);
   const [programa, setPrograma] = React.useState("Kobudo");
-
+  const [showUpButton, setShowUpButton] = useState(false);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
-      const { data, error } = await supabase.from("Alumnos").select("*").eq('Activo', true).filter("Grupo", "eq", active).order("Nickname", { ascending: true });
+      const { data, error } = await supabase
+        .from("Alumnos")
+        .select("*")
+        .eq("Activo", true)
+        .filter("Grupo", "eq", active)
+        .order("Nickname", { ascending: true });
       if (error) {
         console.log(error);
       } else {
@@ -22,8 +27,31 @@ function Calificar() {
     fetchData();
   }, [active]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        setShowUpButton(containerRef.current.scrollTop > 200);
+      }
+    };
+
+    const container = containerRef.current;
+    container.addEventListener("scroll", handleScroll);
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const goUp = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="bg-black h-screen w-full flex flex-col items-center gap-6 overflow-auto p-5 pb-20">
+    <div
+      ref={containerRef}
+      className="main bg-black h-screen w-full flex flex-col items-center gap-6 overflow-auto p-5 pb-20"
+    >
       <select
         className="select select-bordered w-60 max-w-xs text-center"
         value={programa}
@@ -45,6 +73,15 @@ function Calificar() {
           programa={programa}
         />
       ))}
+
+      {showUpButton && (
+        <button
+          onClick={goUp}
+          className="rounded-full p-2 bg-RojoKumo text-white mb-40"
+        >
+          Up
+        </button>
+      )}
     </div>
   );
 }
